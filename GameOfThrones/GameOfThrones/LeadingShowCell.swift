06b65cfee8f585573aa1866aec106d196eeb2973
john Doe <jeffreyalmonte@pursuits-macbook-pro-106.local>
2018-11-20 17:43:19 -0500

    outlets

---
+++ GameOfThrones/GameOfThrones/LeadingShowCell.swift
@@ -0,0 +1,29 @@
+//
+//  LeadingShowCell.swift
+//  GameOfThrones
+//
+//  Created by Jeffrey Almonte on 11/20/18.
+//  Copyright © 2018 Pursuit. All rights reserved.
+//
+
+import UIKit
+
+class LeadingShowCell: UITableViewCell {
+
+      @IBOutlet weak var leadingImage: UIImageView!
+        @IBOutlet weak var leadingEpisodeName: UILabel!
+    @IBOutlet weak var leadingEpisodeNumber: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- GameOfThrones/GameOfThrones/TrailingShowCell.swift
+++ GameOfThrones/GameOfThrones/TrailingShowCell.swift
@@ -1,5 +1,5 @@
 //
-//  EpisodeCell.swift
+//  TrailingShowCell.swift
 //  GameOfThrones
 //
 //  Created by Jeffrey Almonte on 11/20/18.
@@ -8,14 +8,12 @@
 
 import UIKit
 
-class EpisodeCell: UITableViewCell {
-
-    @IBOutlet weak var leadingImage: UIImageView!
+class TrailingShowCell: UITableViewCell {
     @IBOutlet weak var trailingImage: UIImageView!
-    @IBOutlet weak var leadingEpisodeName: UILabel!
     @IBOutlet weak var trailingEpisodeName: UILabel!
-    @IBOutlet weak var leadingEpisodeNumber: UILabel!
     @IBOutlet weak var trailingEpisodeNumber: UILabel!
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
